71081d2b0f4cd3568fc518ca706caf43bb372fbb
Mandy Torrence <mandolinhaynes@gmail.com>
2020-02-03 00:20:55 -0500

    Most of the code, still working on the rendering loop

--- script.js
+++ script.js
@@ -15,13 +15,74 @@ var hourBlocks = $('#hour-blocks');
 
 function addHourBlocks() {
     for (var i = 0; i < hours.length; i++) {
+        console.log(hours[i]);
         rowDiv = $('<div>').addClass('row').attr('id', 'block' + [i]).appendTo(hourBlocks);
-        timeDiv = $('<div>').text(hours[i]).addClass('col-1 hour time-block');
-        textArea = $('<textarea>').attr("id", [i]).addClass('col-9 input');
+        timeDiv = $('<div>').text(hours[i]).addClass('col-2 hour time-block');
+        textArea = $('<textarea>').attr("id", [i] + " text" + [i]).addClass('col-8 input');
         saveBtn = $('<button>').addClass('col-2 saveBtn saveIcon' + [i]);
         saveIcon = $('<i>').addClass('far fa-save');
         $('#block' + [i]).append(timeDiv, textArea, saveBtn);
         $('.saveIcon' + [i]).append(saveIcon);
     }
 }
-addHourBlocks()
+addHourBlocks();
+
+$(document).ready(function () {
+    colorChange();
+    renderText();
+});
+
+//Adding the color change option to the rows based on the time of day
+function colorChange() {
+
+    var realTime = parseInt(moment().hours()) - 9;
+    console.log("Current Time ID: " + realTime);
+
+    $(".input").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        console.log(timeTest);
+
+        if (realTime > timeTest) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (realTime < timeTest) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } else {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    });
+};
+
+//Rendering the saved text
+function renderText() {
+    console.log("Rendering function is running");
+    hourInfo = $(this).siblings(".hour").text();
+    for (var j = 0; j < hours.length; j++) {
+        console.log("Hours: " + hours[j]);
+        var saveWords = (localStorage.getItem(hourInfo));
+        console.log(saveWords);
+        console.log("#text" + [j]);
+        var defaultValue = ("")
+        $("#text" + [j]).val(defaultValue)
+        $("#text" + [j]).val(saveWords)
+    }
+};
+
+//Adding the save Button
+$(".saveBtn").click(function () {
+    var realTime = parseInt(moment().hours()) - 9;
+    console.log("Current Time ID: " + realTime);
+    words = $(this).siblings(".input").val();
+    console.log("words: " + words);
+    hourInfo = $(this).siblings(".hour").text();
+    console.log("hour info: " + hourInfo);
+    localStorage.setItem(hourInfo, JSON.stringify(words));
+
+    colorChange();
+    renderText();
+});
